514598bd7c7fe044ffceed702bdb1042bea7b743
alpaslangul <malpaslangul@gmail.com>
2013-10-24 18:35:16 +0300

    slider eklendi, sayfalara devam edildi.

--- cccotedivoireturkey/html/js/script.js
+++ cccotedivoireturkey/html/js/script.js
@@ -1,14 +1,53 @@
 CCC = {
+    id : 1,
+    imageW : 824,
+
     commonInit : function() {
         $('#mainSearch').bind('click', CCC.clear);
+        $('.spnsLeftBtn').on('click', CCC.slider.leftClick);
+        $('.spnsRightBtn').on('click', CCC.slider.rightClick);
     },
 
     clear : function() {
         $('#mainSearch').attr('value', '');
-    }
+    },
 
+    slider : {
+
+
+        timer : function () {
+            var t = 0;
+            setInterval(function()
+            {
+                CCC.slider.rightClick()
+            }, 3000);
+        },
+
+        leftClick : function ()
+        {
+            if (CCC.id < 4 && CCC.id > 1)
+            {
+                $('.sponsorsContainer').animate({marginLeft: -CCC.imageW * (CCC.id-2) }, 1000, function () { });
+                CCC.id--
+            }
+        },
+
+        rightClick : function ()
+        {
+            if (CCC.id < 3)
+            {
+                $('.sponsorsContainer').animate({marginLeft: -CCC.imageW * CCC.id}, 1000, function () { });
+                CCC.id++
+
+            } else if (CCC.id = 3) {
+                $('.sponsorsContainer').animate({marginLeft: 0}, 1000, function () { });
+                CCC.id = 1;
+            }
+        }
+    }
 }
 
 $(document).ready( function() {
     CCC.commonInit();
+    CCC.slider.timer();
 })
