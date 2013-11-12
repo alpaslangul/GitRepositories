c565068c5f46949cd8b193dac3c8b76ae30643f6
alpaslan@manifakture.com <Alphi123>
2013-11-13 01:40:20 +0200

    panel front-end devam, vector dosyaları taşındı

--- cccotedivoireturkey/html/cccpanel/js/script.js
+++ cccotedivoireturkey/html/cccpanel/js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
     var h = $(document).height();
+    var w = $(document).width();
+    console.log(w);
+    layerWidth = parseInt(w)-545;
     $('.adminLeft').height(h);
+    console.log(layerWidth+'px');
+    $('.adminRight').width(layerWidth+'px');
+})
+
+$(document).resize(function(){
+    var h = $(document).height();
+    var w = $(document).width();
+    console.log(w);
+    layerWidth = parseInt(w)-545;
+    $('.adminLeft').height(h);
+    console.log(layerWidth+'px');
+    $('.adminRight').width(layerWidth+'px');
 })
