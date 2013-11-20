a29503388bbfc76b2bc97f80ce0d6a6eb8bf0457
alpaslangul <malpaslangul@gmail.com>
2013-11-20 14:58:30 +0200

    login güncelleme

---
+++ cccotedivoireturkey/cccpanel/edit.php
@@ -0,0 +1,60 @@
+<?php
+
+ session_start();
+ $check = (int)$_SESSION['status'];
+ if ($check != 1 ){
+     header("Location: login.php");
+ }
+ include 'header.php';
+?>
+
+<!--<!DOCTYPE html>-->
+<!--<head>-->
+<!--    <title></title>-->
+<!--    <meta charset="utf-8">-->
+<!--    <link rel="stylesheet" href="css/style.css">-->
+<!--</head>-->
+<!--<body >-->
+
+<div class="headInf">
+    <div class="information">
+        <div class="leftTop">
+            <span>www.tacpromosyon.com.tr</span>
+        </div>
+
+        <div class="dashBoard">
+            <a href="post-new.php">+ Yeni Haber Ekle</a>
+        </div>
+</div>
+</div>
+<div class="panelRight">
+    <div class="adminLeft">
+        <div class="adminLogo">
+            <img src="img/logo.png" width="50" height="50" alt="logo"/>
+            <span>Chambre De Commerce Et D’ındustrıe
+                  Turque En Cote D’ıvoıre</span>
+        </div>
+        <div class="menuItems"><a href="#" id="users"><span>Kullanıcılar</span></a></div>
+        <div class="menuItems"><a href="#" id="cetegory"><span>Kategoriler</span></a></div>
+        <div class="menuItems"><a href="edit.php" id="news"><span>Haberler</span></a></div>
+        <div class="menuItems"><a href="#" id="gallery"><span>Foto Galeri</span></a></div>
+        <div class="menuItems"><a href="#" id="photo"><span>Fotoğraflar</span></a></div>
+        <div class="menuItems"><a href="#" id="connection"><span>İletişim Formu</span></a></div>
+        <div class="menuItems"><a href="#" id="members"><span>Üyeler</span></a></div>
+        <div class="menuItems"><a href="#" id="settings"><span>Ayarlar</span></a></div>
+
+    </div>
+    <div class="adminRight">
+        <div class="panelHeadText">
+            <a href="#"></a>
+            <span>Başlık Metni</span>
+            <div class="panelbox">
+
+            </div>
+        </div>
+    </div>
+</div>
+    <script type="text/javascript" src="../js/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
